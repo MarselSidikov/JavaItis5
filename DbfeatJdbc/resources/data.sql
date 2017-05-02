7db37358ee982cc627ddb78097f4820fa172339f
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-05-02 20:56:10 +0300

    add all

--- DbfeatJdbc/resources/data.sql
+++ DbfeatJdbc/resources/data.sql
@@ -4,3 +4,36 @@ INSERT INTO group_user(login, password, name, age)
 VALUES ('Ayaz', 'qwerty008', 'Аяз', 25);
 INSERT INTO group_user(login, password, name, age)
 VALUES ('Vika', 'qwerty009', 'Вика', 22);
+INSERT INTO group_user (login,password,name,age)
+VALUES ('Artur','qwerty011','Артур',23);
+INSERT INTO group_user (login,password,name,age)
+VALUES ('Marat','qwerty012','Марат',24);
+INSERT INTO group_user (login,password,name,age)
+VALUES ('Ayvar','qwerty013','Айвар',24);
+INSERT INTO group_user (login,password,name,age)
+VALUES ('Robert','qwerty014','Роберт',18);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+
+VALUES ('Moskvich', 'White', 100000.0, TRUE, 8);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('Mersedes', 'Black', 0, FALSE , 7);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('RollsRoyce', 'Black', 0, FALSE , 16);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('VAZ2115', 'White', 147.2, TRUE , 16);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('VAZ2114', 'Black', 15, TRUE , 16);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('VAZ2109', 'White', 777.0, FALSE , 15);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('Granta', 'Blue', 0, FALSE , 14);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('Honda', 'Black', 147.2, TRUE , 13);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('Mazda3', 'Yelow', 3, TRUE , 12);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('Mustang', 'Red', 0, FALSE , 12);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('BMV', 'Blue', 11.2, TRUE , 11);
+INSERT INTO auto(model, color, carmileage, used, owner_id)
+VALUES ('Toyota', 'Red', 147.2, TRUE , 11);
