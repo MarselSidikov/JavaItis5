e1fca55763bf09c7c041bb4785e8879a42c62251
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-06-24 14:56:32 +0300

    add sec

--- RestSecurity/src/main/java/ru/itis/security/filter/TokenAuthFilter.java
+++ RestSecurity/src/main/java/ru/itis/security/filter/TokenAuthFilter.java
@@ -24,7 +24,6 @@ public class TokenAuthFilter extends GenericFilterBean {
 
     private AuthenticationManager authenticationManager;
 
-
     public TokenAuthFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
