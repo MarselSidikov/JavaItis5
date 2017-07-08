1ec6706fb7a0795770d41d2271a63a1ad510609f
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-07-08 19:21:08 +0300

    add chat

--- RestSecurity/src/main/java/ru/itis/dao/ChatsDao.java
+++ RestSecurity/src/main/java/ru/itis/dao/ChatsDao.java
@@ -6,6 +6,7 @@ import ru.itis.model.Chat;
 
 public interface ChatsDao extends JpaRepository<Chat, Integer> {
 
-    @Query(value = "SELECT COUNT (*) FROM chat_user WHERE user_id = ?1 AND chat_id = ?2", nativeQuery = true)
+    @Query(value = "SELECT COUNT (*) > 0 FROM chat_user WHERE user_id = ?1 " +
+            "AND chat_id = ?2", nativeQuery = true)
     boolean isUserInChat(int userId, int chatId);
 }
