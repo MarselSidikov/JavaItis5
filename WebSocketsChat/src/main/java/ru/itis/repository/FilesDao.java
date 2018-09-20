3b49ba39094ce9c48d6931cda33444cbaf7b50cc
MarselSidikov <sidikov.marsel@gmail.com>
2018-09-20 10:29:50 +0300

    fix chat

--- WebSocketsChat/src/main/java/ru/itis/repository/FilesDao.java
+++
@@ -1,8 +0,0 @@
-package ru.itis.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.itis.model.File;
-
-public interface FilesDao extends JpaRepository<File, Integer> {
-    File findFirstByStorageName(String name);
-}
